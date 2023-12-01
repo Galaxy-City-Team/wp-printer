92a68ac06aecb71dcc529af0dcaee9624f980e66
Domi <106663559+easydomii@users.noreply.github.com>
2023-12-01 22:54:33 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ author 'BackSH00TER - Waypoint RP'
 version '1.0.0'
 
 shared_script {
-    -- '@ox_lib/init.lua', -- Uncomment this if you are planning to use any ox scripts (such as ox notify)
+    '@ox_lib/init.lua', -- Uncomment this if you are planning to use any ox scripts (such as ox notify)
     'shared/config.lua',
     'shared/framework.lua',
 }
